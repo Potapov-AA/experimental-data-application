92cb9b6caad0a0e28d3f2d4a2e4a1bdabf408d74
Potapov Alexander <jestterr@mail.ru>
2022-12-01 14:30:54 +0300

    added filter-function

--- processing.py
+++ processing.py
@@ -5,12 +5,14 @@ import numpy as np
 import math
 from model import Model
 from config import Config
+from analysis import Analysis
 
 
 class Processing():
     def __init__(self):
         self.model = Model()
         self.parametrs = Config()
+        self.analysis = Analysis()
 
     def antiShift(self, data=[i for i in range(1000)]):
 
@@ -164,3 +166,182 @@ class Processing():
             plt.title("Изменение стандартного отклонения")
             
         plt.show()
+    
+    def lpf(self, draw = False):
+        fc1=float(self.parametrs.GetParametr("Parametrs", "fc"))
+        fc2=float(self.parametrs.GetParametr("Parametrs", "fc1"))
+        fc3=float(self.parametrs.GetParametr("Parametrs", "fc2"))
+        fc = [fc1, fc2, fc3]
+        m = int(self.parametrs.GetParametr("Parametrs", "m for lpf"))
+        dt = float(self.parametrs.GetParametr("Parametrs", "dt"))
+        
+        # rectangular part weights
+        D = [0.35577019, 0.2436983, 0.07211497, 0.00630165]
+        result = []
+        
+        if draw:
+            plt.figure(figsize=(10,10))
+        
+        for f in range(len(fc)):
+            lpw = []
+            fact = 2 * fc[f] * dt
+            lpw.append(fact)
+            
+            arg = np.pi * fact
+            
+            for i in range(1, m+1):
+                lpw.append(np.sin(arg*i) / (np.pi*i))
+            
+            # trapezoid smoothing at the end
+            lpw[m] /= 2
+            
+            # P310 smoothing window
+            sumg = lpw[0]
+            for i in range(1, m+1):
+                sum = D[0]
+                arg = np.pi * i / m
+                for k in range(1, 4):
+                    sum += 2 * D[k] * np.cos(arg * k)
+                lpw[i] *= sum
+                sumg += 2 * lpw[i]
+            
+            for i in range(m+1):
+                lpw[i] /= sumg
+            
+            
+            
+            
+            reverseLPW = lpw[::-1]
+            simetricLPW = reverseLPW + lpw
+            result.append(simetricLPW)
+            
+            if draw:
+                plt.subplot(2,3,f+1)
+                plt.title(f"ФНЧ fc = {fc[f]}")
+                plt.plot([i for i in range(len(lpw))], lpw)
+                
+                plt.subplot(2,3,f+4)
+                plt.plot([i for i in range(len(simetricLPW))], simetricLPW)
+            
+        if draw:
+            plt.show()
+        
+        return result   
+    
+    
+    def hpf(self, draw=False):
+        m = int(self.parametrs.GetParametr("Parametrs", "m for lpf"))
+        llpw = self.lpf()
+        loper = 2*m+1
+        
+        if draw:
+            plt.figure(figsize=(10,10))
+        
+        i = 1
+        result = []
+        for lpw in llpw:
+            hpw = []
+            for k in range(loper+1):
+                if k == m:
+                    hpw.append(1 - lpw[k])
+                else:
+                    hpw.append(-lpw[k])
+            if draw:
+                plt.subplot(1,3,i)
+                plt.plot([i for i in range(len(hpw))], hpw)
+            i += 1
+            result.append(hpw)
+        
+        if draw:
+            plt.show()
+        
+        return result
+        
+    def bpf(self, draw = False):
+        m = int(self.parametrs.GetParametr("Parametrs", "m for lpf"))
+        lpw = self.lpf()
+        lpw1 = lpw[1]
+        lpw2 = lpw[2]
+        loper = 2*m+1
+        bpw = []
+        
+        for k in range(loper+1):
+            bpw.append(lpw2[k]-lpw1[k])
+        if draw:
+            plt.figure(figsize=(10,10))
+            plt.title("Полосовой фильтр ПФ")
+            plt.plot([i for i in range(len(bpw))], bpw)
+            plt.show()
+        
+        return bpw
+    
+    def bsf(self, draw = False):
+        m = int(self.parametrs.GetParametr("Parametrs", "m for lpf"))
+        lpw = self.lpf()
+        lpw1 = lpw[1]
+        lpw2 = lpw[2]
+        loper = 2*m+1
+        bsw = []
+        
+        for k in range(loper+1):
+            if k == m:
+                bsw.append(1 + lpw1[k] - lpw2[k])
+            else:
+                bsw.append(lpw1[k] - lpw2[k])
+        if draw:
+            plt.figure(figsize=(10,10))
+            plt.title("Режекторный фильтр РФ")
+            plt.plot([i for i in range(len(bsw))], bsw)
+            plt.show()    
+
+        return bsw
+        
+    def FilterFourier(self):
+        m = int(self.parametrs.GetParametr("Parametrs", "m for lpf"))
+        
+        lpw = self.lpf()[0]
+        hpw = self.hpf()[0]
+        bpw = self.bpf()
+        bsw = self.bsf()
+        
+        loper = 2 * m + 1
+        
+        plt.figure(figsize=(10, 10))
+        
+        lpwFourier = self.analysis.getSpectrFourier(lpw)
+        dataX = [i for i in range(len(lpwFourier))]
+        dataY = [i * loper for i in lpwFourier]
+        
+        plt.subplot(2, 2, 1)
+        plt.title("LPF")
+        plt.plot(dataX, dataY)
+        
+        
+        hpwFourier = self.analysis.getSpectrFourier(hpw)
+        dataX = [i for i in range(len(hpwFourier))]
+        dataY = [i * loper for i in hpwFourier]
+        
+        plt.subplot(2, 2, 2)
+        plt.title("HPF")
+        plt.plot(dataX, dataY)
+        
+        
+        bpwFourier = self.analysis.getSpectrFourier(bpw)
+        dataX = [i for i in range(len(bpwFourier))]
+        dataY = [i * loper for i in bpwFourier]
+        
+        plt.subplot(2, 2, 3)
+        plt.title("BPF")
+        plt.plot(dataX, dataY)
+        
+        
+        bswFourier = self.analysis.getSpectrFourier(bsw)
+        dataX = [i for i in range(len(bswFourier))]
+        dataY = [i * loper for i in bswFourier]
+        
+        plt.subplot(2, 2, 4)
+        plt.title("BSF")
+        plt.plot(dataX, dataY)
+        
+        plt.show()
+        
