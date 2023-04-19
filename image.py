52e5ba4723757b772356a5ea1c70428282bfeeb3
JESTER <jestterr@mail.ru>
2023-04-19 16:09:10 +0300

    Обновление класса
    1) Добавлены комментарии
    2) Убраны лишние методы: (save_last_image, save_image_at_index) теперь метод просто называется save_image; (show_last_image, show_image_at_index) теперь метод называется show_image
    3) Обновлен метод add_updated_data_to_list. Теперь данные добавляются в список
    4) Добавлены методы get_last_index, __generate_default_iamge, get_last_data
    5) Добавлен класс TransformImage, для изменения изображений

--- image.py
+++ image.py
@@ -2,54 +2,111 @@ import re
 import PIL.Image as PilImage
 import numpy as np
 from tkinter import filedialog as fd 
+from matplotlib import pyplot as plt
 
 class Image:
     def __init__(self, path = None) -> None:
-        self.dataImage = self.__read_image(path)
-        
         self.dataImageList = []
-        self.dataImageList.append(self.dataImage)
-        
-        ### ТЕСТОВАЯ, УБРАТЬ КОГДА ВСЕ ОТКРЫТИЯ ФАЙЛА СДЕЛАЮ
-        print(self.dataImageList)
+        if path != None:
+            self.dataImageList.append(self.__read_image(path))
+        else:
+            self.dataImageList.append(self.__generate_default_iamge())
+            
     
-    
-    def save_last_image(self):
+    def add_updated_data_to_list(self, dataImage):
         """
-            Функция для сохранения последнего изображения.
+            Добавляет новое изображение в список
+            
+        Args:
+            dataImage (np.array): массив numpy формата [[0 0 0 0 ... 0 0 0]]
+        """
+        self.dataImageList.append(dataImage)
+    
+    
+    def get_last_data(self):
+        """
+            Возвращает последние данные из списка
+            
+        Returns:
+            np.array: массив numpy формата [[0 0 0 0 ... 0 0 0]]
+        """
+        return self.dataImageList[len(self.dataImageList) - 1]
+    
+    
+    def get_last_index(self):
+        """
+            Возвращает последний индекс списка изображений
+
+        Returns:
+            int: последний индекс списка изображений
+        """
+        return len(self.dataImageList) - 1
+    
+    
+    def save_image(self, index=-1):
+        """
+            Функция для сохранения изображения из списка по индексу.
             Поддерживаются следующие расширения: jpg, png
+            
+        Args:
+            index (int, optional): индекс массива для отображение. По умолчанию -1.
         """
+        if index == -1:
+            index = self.get_last_index()
+            
         try:
-            fileName = fd.asksaveasfilename(confirmoverwrite=True, 
-                                            defaultextension="jpg", 
-                                            initialfile="image.jpg", 
-                                            filetypes = (('JPG', '.jpg'), ('PNG', '.png')))
-            fileData = PilImage.fromarray(self.dataImage.astype('uint8'))
-            fileData.save(fileName)
+            path = fd.asksaveasfilename(confirmoverwrite=True, 
+                                        defaultextension="jpg", 
+                                        initialfile="image.jpg", 
+                                        filetypes = (('JPG', '.jpg'), ('PNG', '.png')))
+            
+            fileExtension = re.search(r'\w+$', path)[0]
+            
+            if fileExtension in ['jpg', 'png']:
+                fileData = PilImage.fromarray(self.dataImageList[index].astype('uint8'))
+                fileData.save(path)
+            
         except Exception:
             print(Exception)
     
     
-    def save_image_at_index(self, index=0):
-        pass
-    
-    
-    def add_updated_data_to_list(self):
-        pass
-    
-    
-    def show_last_image(self):
-        pass
-    
-    
-    def show_image_at_index(self):
-        pass
+    def show_image(self, index = -1):
+        """
+            Отображает изображение из списка
+
+        Args:
+            index (int, optional): индекс массива для отображение. По умолчанию -1.
+        """
+        if index == -1:
+            index = self.get_last_index()
+        
+        image = PilImage.fromarray(self.dataImageList[index])
+        
+        width = image.size[0]
+        hight = image.size[1]
+        
+        plt.figure(figsize=(6,6))
+        plt.title(f"Размеры изображения: {width}x{hight}")
+        plt.imshow(image)
+        plt.axis("off")
+        plt.show()
     
     
     def __read_image(self, path):
+        """
+            Получает путь до файла и проверяет расширение, 
+            если расширение допустимого формата, то проводит 
+            считывание и преобразование данных к стандартному формату
+            
+        Args:
+            path (string): путь до файла
+
+        Returns:
+            np.array: массив numpy приведенный к формату [[0 0 0 0 ... 0 0 0]]
+        """
         fileExtension = re.search(r'\w+$', path)[0]
         
-        if fileExtension in ['jpg']:
+        if fileExtension in ['jpg', 'png']:
             try:
                 dataImage = np.array(PilImage.open(path)).astype('int32')
                 
@@ -63,6 +120,15 @@ class Image:
     
     
     def __transform_data_image_to_default(self, dataImage):
+        """
+            Трансформирует стандартный список, в numpy список формата [[0 0 0 0 ... 0 0 0]]
+
+        Args:
+            dataImage (list): список данных, формата [[[0 0 0]]] или [[ 0 0 0 0 ... 0 0 0]] 
+
+        Returns:
+            np.array: массив numpy приведенный к формату [[0 0 0 0 ... 0 0 0]]
+        """
         transformDataImage = []
         
         for line in dataImage:
@@ -74,13 +140,55 @@ class Image:
                     thirdPixel  = pixel[2]
                     
                     if firstPixel == secondPixel == thirdPixel:
+                        # Серое RGB изображение
                         pixelList.append(firstPixel)
                     else:
+                        # Цветное изображение
                         pixelList.append((firstPixel + secondPixel + thirdPixel) // 3)
-                
                 except:
-                    return dataImage
+                    # список уже в формате [[0 0 0 0 ... 0 0 0]]
+                    return np.array(dataImage).astype('int32')
             
             transformDataImage.append(pixelList)
         
         return np.array(transformDataImage).astype('int32')
+    
+    
+    def __generate_default_iamge(self):
+        """
+            Генерирует дефолтное изображение
+        Returns:
+            np.array: массив numpy приведенный к формату [[0 0 0 0 ... 0 0 0]]
+        """
+        sizeBlackSqard = 256
+        sizeWhiteSqard = 100
+        
+        dataImage = np.empty((sizeBlackSqard, sizeBlackSqard))
+        
+        indexForWhiteSqardStart = sizeBlackSqard/2 - sizeWhiteSqard/2
+        indexForWhiteSqardEnd = sizeBlackSqard/2 + sizeWhiteSqard/2
+        
+        for h in range(sizeBlackSqard):
+            for w in range(sizeBlackSqard):
+                if (w >= indexForWhiteSqardStart and w <= indexForWhiteSqardEnd) and (h >= indexForWhiteSqardStart and h <= indexForWhiteSqardEnd):
+                    dataImage[h, w] = 0
+                else:
+                    dataImage[h, w] = 256
+                    
+        return np.array(dataImage).astype('int32')
+
+
+class TransformImage:
+    def do_negative(self, dataImage):
+        L = dataImage.max()
+        
+        height = dataImage.shape[0]
+        weight = dataImage.shape[1]
+        
+        transformData = np.empty((height, weight))
+        
+        for h in range(height):
+            for w in range(weight):
+                transformData[h][w] = L - 1 - dataImage[h][w]
+        
+        return np.array(transformData).astype('int32')
