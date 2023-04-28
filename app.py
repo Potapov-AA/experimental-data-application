07527491aad8579e3543e9aa99fd05484dde6a2a
JESTER <jestterr@mail.ru>
2023-04-28 10:24:48 +0300

    UI для амплитудного спектра Фурье

--- app.py
+++ app.py
@@ -483,6 +483,14 @@ class App(Tk):
             font=self.mainFont
         ).pack(side=LEFT, anchor=N, expand=True, pady=[5, 0], padx=[0, 200])
         
+        Button(
+            parent,
+            text="Расчитать амплитудный спектр Фурье",
+            width = 42,
+            command=lambda: self.analysis_image(AnalysisImageData.calculate_fourier_transform,
+                                                paramOne = int(self.parametrs.GetParametr("ImageParametrs", "derivativesStep"))),
+            font=self.mainFont
+        ).pack(pady=[0, 0])
     
     def work_with_image(self, function, paramOne=None, paramTwo=None):
         if int(self.indexSpinbox.get()) == -1:
